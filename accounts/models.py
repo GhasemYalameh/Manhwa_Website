51ace2e5316c8f090e1bd158be99e8b274b3984f
ghasem <ghasemyalame06@gmail.com>
2025-06-29 20:19:08 +0330

    make unique phone_number

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=11)
+    phone_number = models.CharField(max_length=11, unique=True)
+
+    USERNAME_FIELD = "phone_number"
 
     def __str__(self):
         return self.username
