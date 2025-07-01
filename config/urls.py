cf2fabe442903717867ec6f62ca598f21821ee13
ghasem <ghasemyalame06@gmail.com>
2025-07-01 10:55:51 +0330

    add media & static dirs

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from django.contrib.auth import urls
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='registration/home.html'), name='home'),
     path('account/', include('accounts.urls')),
     path('account/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
