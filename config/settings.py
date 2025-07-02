f884df24fb1cc66d2861d480063a39609b2226f1
ghasem <ghasemyalame06@gmail.com>
2025-07-02 16:49:12 +0330

    add home.html and static files

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS = [BASE_DIR / 'static']  # initial root static
+STATICFILES_DIRS = [BASE_DIR / 'static']  # initial root static
 
 
 # media settings
