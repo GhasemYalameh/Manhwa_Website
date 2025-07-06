c4931fb9614dca1c12ed87ed91f8fd2b8f3afb6c
ghasem <ghasemyalame06@gmail.com>
2025-07-06 23:13:27 +0330

    cusomize admin manhwa

--- manhwas/admin.py
+++ manhwas/admin.py
@@ -1,19 +1,46 @@
 from django.contrib import admin
-from django import forms
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 
 from .models import Manhwa, Episode, Studio, Genre, Rate, View, Comment, CommentReAction
 
 
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'text']
+    extra = 0
+
+
+class EpisodeInline(admin.TabularInline):
+    model = Episode
+    fields = ['number', 'file']
+    extra = 0
+
+
 class ManhwaAdmin(admin.ModelAdmin):
-    list_display = ('en_title', 'season', 'views_count', 'get_genres')
-    # filter_horizontal = ('genres',)
+    list_display = ('en_title', 'season', 'views_count', 'get_genres', 'comments_count')
     autocomplete_fields = ['genres']
-    list_filter = ['genres']
+    list_filter = ['genres', 'day_of_week', 'studio']
     search_fields = ['en_title']
+    inlines = [EpisodeInline, CommentInline]
+
+    def get_queryset(self, request):
+        return super(ManhwaAdmin, self)\
+            .get_queryset(request)\
+            .prefetch_related('genres', 'comments')\
+            .select_related('studio')\
+            .annotate(comments_count=Count('comments'))
 
     def get_genres(self, obj):
         return ', '.join([genre.title for genre in obj.genres.all()])
     get_genres.short_description = 'Genre'
+
+    @admin.display(description='Comments')
+    def comments_count(self, manhwa):
+        url = reverse('admin:manhwas_comment_changelist') + '?' + urlencode({'manhwa_id': manhwa.id})
+
+        return format_html('<a href="{}">{}</a>', url, manhwa.comments_count)
 
 
 class ViewAdmin(admin.ModelAdmin):
