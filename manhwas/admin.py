e896c3be3d29fb37742e7e440a25db094c42cd82
ghasem <ghasemyalame06@gmail.com>
2025-07-07 12:15:41 +0330

    make true related name episodes

--- manhwas/admin.py
+++ manhwas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.db.models import Count
+from django.db.models import Count, OuterRef, Subquery
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
 
@@ -19,7 +19,7 @@ class EpisodeInline(admin.TabularInline):
 
 
 class ManhwaAdmin(admin.ModelAdmin):
-    list_display = ('en_title', 'season', 'views_count', 'get_genres', 'comments_count')
+    list_display = ('en_title', 'season', 'views_count', 'get_genres', 'episodes_count', 'comments_count')
     autocomplete_fields = ['genres']
     list_filter = ['genres', 'day_of_week', 'studio']
     search_fields = ['en_title']
@@ -28,19 +28,39 @@ class ManhwaAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         return super(ManhwaAdmin, self)\
             .get_queryset(request)\
-            .prefetch_related('genres', 'comments')\
+            .prefetch_related('genres')\
             .select_related('studio')\
-            .annotate(comments_count=Count('comments'))
+            .annotate(
+                episodes_count=Subquery(
+                    Episode.objects
+                    .filter(manhwa=OuterRef('pk'))
+                    .values('manhwa')
+                    .annotate(count=Count('id'))
+                    .values('count')
+                ),
+                comments_count=Subquery(
+                    Comment.objects
+                    .filter(manhwa=OuterRef('pk'))
+                    .values('manhwa')
+                    .annotate(count=Count('id'))
+                    .values('count')
+                )
+            )
 
     def get_genres(self, obj):
         return ', '.join([genre.title for genre in obj.genres.all()])
     get_genres.short_description = 'Genre'
 
-    @admin.display(description='Comments')
-    def comments_count(self, manhwa):
-        url = reverse('admin:manhwas_comment_changelist') + '?' + urlencode({'manhwa_id': manhwa.id})
+    @admin.display(description='Episodes', ordering='episodes_count')
+    def episodes_count(self, manhwa):
+        url = reverse('admin:manhwas_episode_changelist') + "?" + urlencode({'manhwa__id': manhwa.id})
+        return format_html('<a href="{}">{}</a>', url, manhwa.episodes_count or 0)
 
-        return format_html('<a href="{}">{}</a>', url, manhwa.comments_count)
+    @admin.display(description='Comments', ordering='comments_count')
+    def comments_count(self, manhwa):
+        url = reverse('admin:manhwas_comment_changelist') + '?' + urlencode({'manhwa__id': manhwa.id})
+
+        return format_html('<a href="{}">{}</a>', url, manhwa.comments_count or 0)
 
 
 class ViewAdmin(admin.ModelAdmin):
