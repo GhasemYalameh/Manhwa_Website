403e4ebb4f4d56ca9d260e70e6478da87395dee0
ghasem <ghasemyalame06@gmail.com>
2025-07-13 14:01:00 +0330

    new url for submit reaction

--- manhwas/urls.py
+++ manhwas/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('detail/<int:pk>/', views.manhwa_detail, name='manhwa_detail'),
     path('manhwa/<int:pk>/add-comment/', views.add_comment_manhwa, name='add_comment_manhwa'),
+    path('comment-reaction/<int:pk>/', views.change_or_create_reaction, name='set_reaction'),
 ]
