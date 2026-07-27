f412ffc4801207e54d42b071fe895b5d6148842b
Ghasem <ghasemyalame06@gmail.com>
2026-07-27 19:12:52 +0330

    deleting static url from config.urls

--- config/urls.py
+++ config/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
+] + debug_toolbar_urls()
 
 
 if 'rosetta' in settings.INSTALLED_APPS:
