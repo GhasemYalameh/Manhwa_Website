aa3f1f77fdba5ddca463434f237b7bdc8ff94e10
Ghasem <ghasemyalame06@gmail.com>
2026-07-28 23:46:13 +0330

    sub migratins & models added to admin

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subscription, SubscriptionOrder, SubscriptionPlan
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_subscriber', 'expire_date',)
+
+@admin.register(SubscriptionOrder)
+class SubscriptionOrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'is_paid', 'created_at',)
+
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'duration', 'price',)
+
