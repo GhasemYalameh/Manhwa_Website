b9de8f56684a44219576c7b7011c38ec9fe9b056
Ghasem <ghasemyalame06@gmail.com>
2026-07-28 22:38:04 +0330

    subscription models comppleted

--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import CustomUser
+
+class SubscriptionPlan(models.Model):
+    name = models.CharField(max_length=100)
+    duration = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Subscription(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='subscription')
+    is_subscriber = models.BooleanField(default=False)
+    last_validation = models.DateField() # last validation for trust of is_subscriber field
+    expire_date = models.DateTimeField()
+
+
+    creation_date = models.DateTimeField()
+
+
+
+class SubscriptionOrder(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='subscription_orders')
+    plan = models.ForeignKey(SubscriptionPlan, on_delete=models.PROTECT, related_name='subscription_orders')
+    is_paid = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
