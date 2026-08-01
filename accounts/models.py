f03cd32321d915a03b0faeb385b8779fa0b9b9ca
Ghasem <ghasemyalame06@gmail.com>
2026-08-01 17:30:40 +0330

    changing Custom user model and deleting last migration files

--- accounts/models.py
+++ accounts/models.py
@@ -1,34 +1,103 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, BaseUserManager
 from django.db import models
 from django.core.validators import RegexValidator
 
 from manhwas.models import Manhwa
 
 
-class CustomUser(AbstractUser):
-    phone_regex = RegexValidator(
-        regex=r'^09\d{9}$',
-        message='شماره موبایل باید 11 رقم و با 09 شروع شود'
-    )
+# class CustomUser(AbstractUser):
+#     phone_regex = RegexValidator(
+#         regex=r'^09\d{9}$',
+#         message='شماره موبایل باید 11 رقم و با 09 شروع شود'
+#     )
 
-    phone_number = models.CharField(
-        validators=[phone_regex],
-        max_length=11,
-        unique=True,
-        verbose_name='شماره موبایل'
-    )
-    watch_list = models.ManyToManyField(Manhwa, blank=True)
+#     phone_number = models.CharField(
+#         validators=[phone_regex],
+#         max_length=11,
+#         unique=True,
+#         verbose_name='شماره موبایل'
+#     )
+#     watch_list = models.ManyToManyField(Manhwa, blank=True)
 
+#     USERNAME_FIELD = 'phone_number'
+#     REQUIRED_FIELDS = ['username']
+
+#     def __str__(self):
+#         try:
+#             return self.username
+#         except:
+#             return self.phone_number
+
+#     def save(self, *args, **kwargs):
+#         if not self.username:
+#             self.username = self.phone_number
+#         super().save(*args, **kwargs)
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        """
+        creating a normal user
+        """
+        if not phone_number:
+            raise ValueError("phone number is required.")
+        if not password:
+            raise ValueError("password is required.")
+
+        user = self.model(phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        """
+        creating an superuser
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("superuser must have is_superuser=True.")
+
+        return self.create_user(phone_number, password, **extra_fields)
+
+    def create_user_via_otp(self, phone_number, password=None, **extra_fields):
+        """"
+        creating an user with unusable password hash. just with phone number
+        """
+        if not phone_number:
+            raise ValueError("phone number is required.")
+
+        user = self.model(phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser):
+    phone_regex = RegexValidator(regex=r'^09\d{9}$',
+        message="mobile format most be 09xxxxxxxxx (11 digits)."
+    )
+    phone_number = models.CharField(max_length=11, unique=True, validators=[phone_regex],)
+
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'phone_number'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []    
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
 
     def __str__(self):
-        try:
-            return self.username
-        except:
-            return self.phone_number
-
-    def save(self, *args, **kwargs):
-        if not self.username:
-            self.username = self.phone_number
-        super().save(*args, **kwargs)
+        return self.phone_number
