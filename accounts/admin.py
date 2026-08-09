27122838c92e10d093318ba228d8b400bbaab833
Ghasem <ghasemyalame06@gmail.com>
2026-08-09 21:37:48 +0330

    bug fix in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     # فیلدهای نمایش در لیست
-    list_display = ['phone_number', 'first_name', 'last_name', 'is_active']
+    list_display = ['phone_number', 'first_name', 'is_staff', 'is_active', 'is_new_user',]
 
     # فیلدهای جستجو
     search_fields = ['phone_number', 'first_name', 'last_name']
@@ -21,7 +21,7 @@ class CustomUserAdmin(UserAdmin):
     # تنظیمات فرم ویرایش
     fieldsets = (
         (None, {'fields': ('phone_number', 'password')}),
-        ('اطلاعات شخصی', {'fields': ('first_name', 'last_name', 'email')}),
+        ('اطلاعات شخصی', {'fields': ('first_name', 'last_name', 'email', 'is_new_user',)}),
         ('دسترسی‌ها', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         # ('manhwa', {'fields': ('watch_list',)}),
         ('تاریخ‌ها', {'fields': ('last_login', 'date_joined')}),
